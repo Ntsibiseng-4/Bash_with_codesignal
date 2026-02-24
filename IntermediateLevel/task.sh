2bd9ff3fa3aae8b539b60eb21537fec1a11b1243
Ntsibiseng <naleshabana@gmail.com>
2026-02-24 14:02:21 +0200

    Update task.sh

--- IntermediateLevel/task.sh
+++ IntermediateLevel/task.sh
@@ -13,14 +13,12 @@ touch result.txt
 echo "Word Counts by Line" > result.txt
 
 # TODO: Read each line from file.txt, count the words, and append the count to result.txt
-cat file.txt >> result.txt
-
 counts=1
-
-while read lines
+while read -r line   # Read each line from file.txt
 do
-echo "$counts"."$lines"
-((counts++))
-done < result.txt
+word_count=$(echo "$line" | wc -w)   # Count the number of words in the current line using wc -w
+echo "$word_count" >> result.txt  # Append line number, original line, and word count to result.txt
+((counts++))  # Increment line counter
+done < file.txt
 
-cat result.txt | wc -w
+cat result.txt
