7ba8f637244a3fba71ee1e32303b5593225727e7
Ntsibiseng <naleshabana@gmail.com>
2026-03-20 11:54:13 +0200

    Create updatingPack.sh

---
+++ System-Automation/updatingPack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to update system packages
+echo "Listing all packages"
+apt list --installed > package_list.txt
+
+echo "Updating package lists..."
+sudo apt-get update > updated_list.txt
+
+echo "Listing packages that will be upgraded..."
+sudo apt-get -s upgrade  > upgrades.txt
